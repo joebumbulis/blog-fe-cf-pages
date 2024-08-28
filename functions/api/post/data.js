ce5a20960666370b30443bc99cebd075b6c2db28
Joe Bumbulis <joebumbulis@gmail.com>
2024-08-28 14:59:18 -0500

    updates

--- functions/api/post/data.js
+++ functions/api/post/data.js
@@ -7,7 +7,7 @@ const posts = [
   },
   {
     id: 2,
-    title: 'Updating my blog',
+    title: 'Updating my blogs',
     text: "It's my second blog post! I'm still writing and publishing using Cloudflare Workers + Pages :)",
     published_at: new Date('2020-10-26'),
   },
